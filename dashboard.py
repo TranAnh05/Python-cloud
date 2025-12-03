cd901942d4a9c1387226683cdf83b8f81738802f
TranAnh05 <anh900472@gmail.com>
2025-12-03 15:26:07 +0700

    update dashboard file

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sqlalchemy import create_engine, text
 
 # --- Cấu hình trang ---
 st.set_page_config(
